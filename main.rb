51e00609c7df82475dfdd6b428cc40a2b13e3f11
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-14 17:04:36 -0300

    fix linter errors and screenshot

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require './MyEnumerable'
+require './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -9,12 +9,11 @@ class MyList
   def each(&block)
     @list.each(&block)
   end
-
 end
 
 list = MyList.new(1, 2, 3, 4)
-list.all? {|e| e < 5}
-list.all? {|e| e > 5}
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
-list.filter {|e| e.even?}
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,19 +5,17 @@ module MyEnumerable
     end
     false
   end
-  
+
   def any?
     return true unless block_given?
 
     each { |item| return true if yield(item) }
     false
   end
-  
-  
-  def filter
-    newArray = []
-    each { |item| newArray.push(item) if yield(item) }
-    newArray
-  end
 
+  def filter
+    new_array = []
+    each { |item| new_array.push(item) if yield(item) }
+    new_array
+  end
 end
